0063c57454fc36ec319594b21554883fcb83f3f3
Stephen Ball <sdball@gmail.com>
2011-10-25 23:35:27 -0400

    added twipsy support

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
   $('#topbar').scrollSpy();
+  $(".twipsies a").each(function () {
+    $(this).twipsy({
+      live: false,
+      placement: $(this).attr('data-placement'),
+      trigger: 'hover',
+      offset: 2
+    })
+    .twipsy();
+  })
 });
